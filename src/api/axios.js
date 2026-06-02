4369567ee5d88fc3719d961c308a0fedf136664c
svayas5237 <svayas5237@uta.edu.ec>
2026-06-02 16:37:51 -0500

    fix BASE_URL production images

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,6 @@ const API = axios.create({
   baseURL: 'https://ecologymuebles-api-dxd2cdezeyfhasau.canadacentral-01.azurewebsites.net/api',
 });
 
-export const BASE_URL = 'https://ecologymuebles-api-dxd2cdezeyfhasau.canadacentral-01.azurewebsites.net/api';
+export const BASE_URL = 'https://ecologymuebles-api-dxd2cdezeyfhasau.canadacentral-01.azurewebsites.net';
 
 export default API;
